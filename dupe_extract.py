d968c5c52707cf8864603a89e3ab7418c18fe4f4
Jeremy Douglass <jeremydouglass@gmail.com>
2019-05-26 10:21:18 -0700

    dupe_extract: continue on bad file, missing metadata

--- dupe_extract.py
+++ dupe_extract.py
@@ -29,31 +29,35 @@ def dupe_extract(zip_dir_root='', output_dir='dupe_inspect'):
     # loop over zips and unpack for editing
     for zip_file in zip_files:
         print("\n---\nOpening:", zip_file)
+        try:
+            with zipfile.ZipFile(zip_file, 'r') as zfile:
+                # create per-zip output subdirectory for duplicate json examples
+                zip_output_dir = os.path.join(output_dir, os.path.basename(zip_file).rsplit('.zip')[0])
+                os.makedirs(zip_output_dir, exist_ok=True)
+            
+                # duplicates
+                zfile_dupes = io.TextIOWrapper(zfile.open('_duplicates.txt', 'r')).readlines()
+                print(' ...copying ', len(zfile_dupes), 'duplicate pairs')
+                dupes_all.extend(zfile_dupes)
+                zfile.extract('_duplicates.txt', path=zip_output_dir, pwd=None)
 
-        with zipfile.ZipFile(zip_file, 'r') as zfile:
-            # create per-zip output subdirectory for duplicate json examples
-            zip_output_dir = os.path.join(output_dir, os.path.basename(zip_file).rsplit('.zip')[0])
-            os.makedirs(zip_output_dir, exist_ok=True)
-
-            # duplicates
-            zfile_dupes = io.TextIOWrapper(zfile.open('_duplicates.txt', 'r')).readlines()
-            print(' ...copying ', len(zfile_dupes), 'duplicate pairs')
-            dupes_all.extend(zfile_dupes)
-            zfile.extract('_duplicates.txt', path=zip_output_dir, pwd=None)
-
-            # examples -- copy duplicate json examples to subdirectory
-            print(' ...copying duplicates to subfolder: ', zip_output_dir)
-            for zdupe_row in zfile_dupes:
-                zdupe_row = zdupe_row.split('\t')
-                zfile.extract(zdupe_row[1].strip(), path=zip_output_dir, pwd=None)
-                zfile.extract(zdupe_row[2].strip(), path=zip_output_dir, pwd=None)
-        
-            # deletes
-            zfile_deletes = io.TextIOWrapper(zfile.open('_deletes.txt', 'r')).readlines()
-            print(' ...copying delete recommendations')
-            for zd in zfile_deletes:
-                deletes_all.append(zd)
-            zfile.extract('_deletes.txt', path=zip_output_dir, pwd=None)
+                # examples -- copy duplicate json examples to subdirectory
+                print(' ...copying duplicates to subfolder: ', zip_output_dir)
+                for zdupe_row in zfile_dupes:
+                    zdupe_row = zdupe_row.split('\t')
+                    zfile.extract(zdupe_row[1].strip(), path=zip_output_dir, pwd=None)
+                    zfile.extract(zdupe_row[2].strip(), path=zip_output_dir, pwd=None)
+            
+                # deletes
+                zfile_deletes = io.TextIOWrapper(zfile.open('_deletes.txt', 'r')).readlines()
+                print(' ...copying delete recommendations')
+                for zd in zfile_deletes:
+                    deletes_all.append(zd)
+                zfile.extract('_deletes.txt', path=zip_output_dir, pwd=None)
+            
+        # if the zip file is invalid, or if the metadata files are missing.
+        except (zipfile.BadZipFile, KeyError) as err:
+            print(err.__class__.__name__, ": ", zip_file, err)
 
     # save combined duplicate pairs
     with open(os.path.join(output_dir,'_duplicates_all.txt'), "w") as dupefile:
